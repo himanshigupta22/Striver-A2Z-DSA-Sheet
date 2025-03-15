3099360ff73c4153b926142ce3546e7ac064003f
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-03-15 23:15:34 +0530

    Create 42. Trapping Rain Water.cpp

---
+++ 42. Trapping Rain Water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int maxL[n];
+        int maxR[n];
+        int sum = 0;
+
+        maxL[0] = height[0];
+        maxR[n-1] = height[n-1];
+       
+        for(int i=1; i < n; i++){
+          maxL[i] = max(maxL[i-1], height[i]);
+        }
+
+        for(int i=n-2; i >= 0; i--){
+          maxR[i] = max(maxR[i+1], height[i]);
+        }
+
+        for(int i=0; i < n; i++){
+           sum += min(maxR[i], maxL[i]) - height[i];
+        }
+
+        return sum;
+    }
+};
