859ca36c990ed3e2aa871e569f93ab1b0dc57690
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-04-06 17:05:01 +0530

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()) return false;
+
+        unordered_map<char, int> mpp1;
+        unordered_map<char, int> mpp2;
+
+        for(int i=0; i < s.length(); i++){
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
+        }
+        if(mpp1 == mpp2) return true;
+
+        return false;
+
+    }
+};
