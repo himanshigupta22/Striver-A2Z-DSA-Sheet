1ff510c5f10e862c90b58e7b8bca94f65073f0a8
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-04-06 10:24:54 +0530

    Create 205. Isomorphic Strings.cpp

---
+++ 205. Isomorphic Strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> mpp1;
+        unordered_map<char, char> mpp2;
+
+        for(int i=0; i < s.length(); i++){
+            // s -> t 
+            if(mpp1.find(s[i]) == mpp1.end()){
+              mpp1[s[i]] = t[i];
+            } else {
+                if(mpp1[s[i]] != t[i]) return false;
+            }
+
+            // t -> s
+            if(mpp2.find(t[i]) == mpp2.end()){
+              mpp2[t[i]] = s[i];
+            } else {
+                if(mpp2[t[i]] != s[i]) return false;
+            }
+        }
+        return true;
+    }
+};
