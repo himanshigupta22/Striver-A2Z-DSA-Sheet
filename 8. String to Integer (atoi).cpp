061f0b2a2610f76bd4177da2f7eab8e682a3e805
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-04-07 16:33:04 +0530

    Create 8. String to Integer (atoi).cpp

---
+++ 8. String to Integer (atoi).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0; 
+        int n = s.length();
+
+        while(i < n && s[i] == ' '){
+          i++;
+        }
+        
+        int sign = 1;
+        if(i < n && (s[i] == '-' || s[i] == '+')){
+            if(s[i] == '-') sign = -1;
+            i++;
+        }
+
+        long long result = 0;
+        while(i < n && isdigit(s[i])){
+            result = result*10 + (s[i] - '0');
+
+            if(sign*result <= INT_MIN) return INT_MIN;
+            if(sign*result >= INT_MAX) return INT_MAX;
+            i++;
+        }
+
+        return (int)(sign*result);
+    }
+};
