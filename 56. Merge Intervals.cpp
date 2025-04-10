b0f48e18402e69f216b72fd269a67ca2e016ecea
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-04-10 17:27:48 +0530

    Create 56. Merge Intervals.cpp

---
+++ 56. Merge Intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        int i = 0;
+
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[i]);
+
+        for(int i=1; i < n; i++){
+           if(ans.back()[1] >= intervals[i][0]){
+              ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+           } else {
+              ans.push_back(intervals[i]);
+           }
+        }
+        return ans;
+    }
+};
