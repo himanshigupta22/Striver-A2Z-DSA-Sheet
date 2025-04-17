576b6e03d8cffc31fadd547bc5f356cd9fddf98d
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2025-04-17 16:32:44 +0530

    Create 455. Assign Cookies.cpp

---
+++ Greedy/455. Assign Cookies.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int i=0, j=0;
+        int count=0;
+        sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+
+        while(i < g.size() && j < s.size()){
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+            } 
+            j++;
+        }
+        return i;
+    }
+};
